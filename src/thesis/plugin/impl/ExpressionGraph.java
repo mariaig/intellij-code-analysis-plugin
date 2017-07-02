2064a1786819535df2ccd679a52bbaa9fcb35a49
Maria Igescu <maria.igescu@gmail.com>
2017-07-02 13:30:24 +0300

    added notice to the header of each class

--- src/thesis/plugin/impl/ExpressionGraph.java
+++ src/thesis/plugin/impl/ExpressionGraph.java
@@ -3,7 +3,19 @@ package thesis.plugin.impl;
 import java.util.*;
 
 /**
- * Created by Maria on 6/4/2017.
+ * Copyright Maria Igescu
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class ExpressionGraph {
     String expression;
@@ -22,7 +34,6 @@ public class ExpressionGraph {
     }
 
 
-
     public boolean containsDirectChain(String node1, String node2) {
         for (Map.Entry<String, String> edge : edges.entrySet()) {
             String key = aliases.get(edge.getKey());
@@ -39,13 +50,13 @@ public class ExpressionGraph {
     }
 
     public List<String> getParametersFor(String node) {
-        if(node == null || node.isEmpty()) {
+        if (node == null || node.isEmpty()) {
             return new ArrayList<>();
         }
         List<String> returnList = new ArrayList<>();
 
-        for(Map.Entry<String, String> entry : methodParametersMap.entrySet()) {
-            if(node.equals(aliases.get(entry.getKey()))) {
+        for (Map.Entry<String, String> entry : methodParametersMap.entrySet()) {
+            if (node.equals(aliases.get(entry.getKey()))) {
                 String exactedValueWithoutBraces = entry.getValue().substring(1, entry.getValue().length() - 1);
                 returnList.add(exactedValueWithoutBraces);
             }
@@ -74,10 +85,10 @@ public class ExpressionGraph {
             data.addArgument(exactValueWithoutBraces);
         } else if (parentNode.equals(node) && childNode.equals(node)) {
             data.addArgument(exactValueWithoutBraces);
-            if(!nextChildNode.equals(node)) {
+            if (!nextChildNode.equals(node)) {
                 // at the end of the chain
                 data.setLastIdInChain(childId);
-                if(!nextChildNode.isEmpty()) {
+                if (!nextChildNode.isEmpty()) {
                     data.setLastChildId(nextChildId);
                 }
                 return;
--- src/thesis/plugin/inspection/ParallelStreamInspection.java
+++ src/thesis/plugin/inspection/ParallelStreamInspection.java
@@ -21,7 +21,19 @@ import static java.util.stream.Collectors.toList;
 import static thesis.plugin.impl.Constants.PLUGIN_GROUP;
 
 /**
- * Created by Maria on 5/30/2017.
+ * Copyright Maria Igescu
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class ParallelStreamInspection extends BaseJavaLocalInspectionTool {
     private static final Logger LOG = Logger.getInstance(ParallelStreamInspection.class);
